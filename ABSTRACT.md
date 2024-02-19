0b605c9d1606e8398aa54dba72d365e97640db56
peixed351 <peixed351@gmail.com>
2024-02-19 16:13:17 +0300

    add settings

--- ABSTRACT.md
+++ ABSTRACT.md
@@ -1,5 +1,33 @@
+The **POG: People On Grass Dataset** is a real-world dataset. It shows people on mostly grassy areas from various angles and altitudes between 4 and 103 m. It contains 13,713 objects in 2900 images. The images are taken with a resolution of 3840 × 2150 px and also come with meta data like GPS location as well as
+UAS altitude and attitude.
 
-Sample image template:
-<img src="https://github.com/dataset-ninja/gland-segmentation/assets/78355358/f158d0dd-71d5-41a2-aba5-4a5f57d54c35" alt="image" width="800">
+## Motivation
 
-<span style="font-size: smaller; font-style: italic;">Image description.</span>
+Detection of objects from Unmanned Aerial Vehicles (UAVs) has emerged as a significant area of research due to its wide-ranging applications, including traffic surveillance, smart city management, and search and rescue operations. Despite notable advancements in generic object detection, the task remains highly challenging when applied to images captured by UAVs, primarily due to the diverse conditions encountered across domains.
+
+One of the foremost challenges lies in the variability of images captured from different altitudes. Objects appear in vastly different scales, ranging from as small as 10 pixels to over 300 pixels, depending on the altitude. Lower altitudes yield detailed images, whereas higher altitudes result in blurrier images with more objects captured. Moreover, UAVs often utilize gimbal cameras, allowing for capturing objects from various viewing angles (pitch axis), further adding to the complexity. The roll axis of a UAV introduces additional variation, leading to objects being captured with diverse aspect ratios and orientations. This variability is particularly pronounced in top-down views, making it challenging to distinguish between similar objects such as cars and vans.
+
+Numerous factors contribute to the appearance of objects, including variations in weather conditions, time of day, GPS location, and camera sensor characteristics. For instance, weather conditions like rain or sunshine affect object illumination, while different times of day can alter visibility. Backgrounds also vary significantly between urban and rural areas, further complicating object detection. Additionally, differences in camera lenses introduce distortions that impact image quality. These variations become even more critical when considering the interaction between different domains, highlighting the complexity of object detection from UAV-captured images.
+
+## Dataset description
+
+The authors have compiled the experimental dataset PeopleOnGrass (POG), which comprises 2,900 images with a resolution of 3840x2160 pixels. These images depict individuals captured from various perspectives and altitudes, ranging from 0° (horizontal) to 90° (top-down), and heights spanning from 4 meters to 103 meters. Each image is meticulously labeled with its precise altitude and angle of capture. Additionally, the dataset includes supplementary metadata such as GPS coordinates, UAV velocity and rotation information, timestamps, and other relevant details.
+
+<img src="https://github.com/dataset-ninja/pog/assets/120389559/931db89b-68fe-4e47-8151-8b75dcc730e7" alt="image" width="1000">
+
+<span style="font-size: smaller; font-style: italic;">Example images of the dataset POG, showing the same scenery taken from different perspectives (top: 10m, 10◦, bottom: 100m, 90◦).</span>
+
+The data collection was conducted using a DJI Matrice 210 drone equipped with a Zenmuse XT2 camera. Metadata was extracted using [DJI's](https://developer.dji.com/onboard-sdk/) onboard software development kit. A metadata stamp is generated for each frame at a rate of 10 hertz to synchronize with the video data captured at 30 frames per second. To ensure temporal alignment between the videos data and timestamps, a nearest neighbor method was applied. For each image/frame, the dataset provides logged data from onboard sensors including the clock, barometer, Inertial Measurement Unit (IMU), and Global Positioning System (GPS) respectively:
+
+* current ***datetime*** of capture,
+* ***latitude***, ***longitude*** and altitude of the UAV,
+* camera pitch, roll and yaw angle (viewing angle),
+* speed along the x-, y and z-axes.
+
+The meta values lie within the error thresholds introduced by the different sensors.
+
+<img src="https://github.com/dataset-ninja/pog/assets/120389559/b7726ff0-eb4a-4e8f-9bef-39d6f7fedd49" alt="image" width="1000">
+
+<span style="font-size: smaller; font-style: italic;">Distribution of objects in PeopleOnGrass (POG) across different levels of altitude and camera pitch angles. For visualization purposes only a 4x10 grid is shown.</span>
+
+The authors manually and carefully annotate 13,713 people. They note that this is a simple real-world data set, suffering from fewer confounders than large data sets which is ideal for testing out the efficacy of multi-modal methods.
--- src/convert.py
+++ src/convert.py
@@ -1,12 +1,17 @@
-import supervisely as sly
+import csv
 import os
-from dataset_tools.convert import unpack_if_archive
-import src.settings as s
-from urllib.parse import unquote, urlparse
-from supervisely.io.fs import get_file_name, get_file_size
 import shutil
+from collections import defaultdict
+from urllib.parse import unquote, urlparse
 
+import supervisely as sly
+from dataset_tools.convert import unpack_if_archive
+from supervisely.io.fs import get_file_name, get_file_name_with_ext
+from supervisely.io.json import load_json_file
 from tqdm import tqdm
+
+import src.settings as s
+
 
 def download_dataset(teamfiles_dir: str) -> str:
     """Use it for large datasets to convert them on the instance"""
@@ -29,7 +34,7 @@ def download_dataset(teamfiles_dir: str) -> str:
             total=fsize,
             unit="B",
             unit_scale=True,
-        ) as pbar:        
+        ) as pbar:
             api.file.download(team_id, teamfiles_path, local_path, progress_cb=pbar)
         dataset_path = unpack_if_archive(local_path)
 
@@ -57,7 +62,8 @@ def download_dataset(teamfiles_dir: str) -> str:
 
         dataset_path = storage_dir
     return dataset_path
-    
+
+
 def count_files(path, extension):
     count = 0
     for root, dirs, files in os.walk(path):
@@ -65,21 +71,126 @@ def count_files(path, extension):
             if file.endswith(extension):
                 count += 1
     return count
-    
+
+
 def convert_and_upload_supervisely_project(
     api: sly.Api, workspace_id: int, project_name: str
 ) -> sly.ProjectInfo:
     ### Function should read local dataset and upload it to Supervisely project, then return project info.###
-    raise NotImplementedError("The converter should be implemented manually.")
+    dataset_path = "/home/alex/DATASETS/TODO/POG/PeopleOnGrass/images"
+    batch_size = 10
 
-    # dataset_path = "/local/path/to/your/dataset" # general way
-    # dataset_path = download_dataset(teamfiles_dir) # for large datasets stored on instance
+    train_anns = "/home/alex/DATASETS/TODO/POG/PeopleOnGrass/instances_train.json"
+    val_anns = "/home/alex/DATASETS/TODO/POG/PeopleOnGrass/instances_val.json"
+    meta_path = "/home/alex/DATASETS/TODO/POG/PeopleOnGrass/meta.csv"
 
-    # ... some code here ...
+    ds_name_to_anns = {"train": train_anns, "val": val_anns}
 
-    # sly.logger.info('Deleting temporary app storage files...')
-    # shutil.rmtree(storage_dir)
+    def create_ann(image_path):
+        labels = []
+        tags = []
 
-    # return project
+        image_name = get_file_name_with_ext(image_path)
+        img_height = image_name_to_shape[image_name][0]
+        img_wight = image_name_to_shape[image_name][1]
 
+        tags_vals = name_to_meta.get(get_file_name_with_ext(image_path))
+        if tags_vals is not None:
+            time = sly.Tag(time_meta, value=int(tags_vals[0]))
+            datetime = sly.Tag(datetime_meta, value=tags_vals[1])
+            latitude = sly.Tag(latitude_meta, value=float(tags_vals[2]))
+            longitude = sly.Tag(longitude_meta, value=float(tags_vals[3]))
+            speed = sly.Tag(speed_meta, value=float(tags_vals[4]))
+            distance = sly.Tag(distance_meta, value=float(tags_vals[5]))
 
+            tags.extend([time, datetime, latitude, longitude, speed, distance])
+
+        # image_np = sly.imaging.image.read(image_path)[:, :, 0]
+        # img_height = image_np.shape[0]
+        # img_wight = image_np.shape[1]
+
+        ann_data = image_id_to_ann_data[get_file_name_with_ext(image_path)]
+        for bbox_coord in ann_data:
+            rectangle = sly.Rectangle(
+                top=int(bbox_coord[1]),
+                left=int(bbox_coord[0]),
+                bottom=int(bbox_coord[1] + bbox_coord[3]),
+                right=int(bbox_coord[0] + bbox_coord[2]),
+            )
+            label_rectangle = sly.Label(rectangle, obj_class)
+            labels.append(label_rectangle)
+
+        return sly.Annotation(img_size=(img_height, img_wight), labels=labels, img_tags=tags)
+
+    project = api.project.create(workspace_id, project_name, change_name_if_conflict=True)
+
+    obj_class = sly.ObjClass("pedestrain", sly.Rectangle)
+
+    time_meta = sly.TagMeta("time", sly.TagValueType.ANY_NUMBER)
+    datetime_meta = sly.TagMeta("datetime", sly.TagValueType.ANY_STRING)
+    latitude_meta = sly.TagMeta("latitude", sly.TagValueType.ANY_NUMBER)
+    longitude_meta = sly.TagMeta("longitude", sly.TagValueType.ANY_NUMBER)
+    speed_meta = sly.TagMeta("speed", sly.TagValueType.ANY_NUMBER)
+    distance_meta = sly.TagMeta("distance", sly.TagValueType.ANY_NUMBER)
+
+    meta = sly.ProjectMeta(
+        obj_classes=[obj_class],
+        tag_metas=[
+            time_meta,
+            datetime_meta,
+            latitude_meta,
+            longitude_meta,
+            speed_meta,
+            distance_meta,
+        ],
+    )
+    api.project.update_meta(project.id, meta.to_json())
+
+    name_to_meta = {}
+
+    with open(meta_path, "r") as file:
+        csvreader = csv.reader(file)
+        for row in csvreader:
+            name_to_meta[row[0]] = [row[1], row[2], row[3], row[4], row[9], row[10]]
+
+    for ds_name, ann_path in ds_name_to_anns.items():
+
+        dataset = api.dataset.create(project.id, ds_name, change_name_if_conflict=True)
+
+        curr_images_path = os.path.join(dataset_path, ds_name)
+
+        json_ann = load_json_file(ann_path)
+        image_id_to_name = {}
+        image_name_to_shape = {}
+        image_id_to_ann_data = defaultdict(list)
+
+        images_data = json_ann["images"]
+        for image_data in images_data:
+            image_id_to_name[image_data["id"]] = image_data["file_name"]
+            image_name_to_shape[image_data["file_name"]] = (
+                image_data["height"],
+                image_data["width"],
+            )
+
+        annotations = json_ann["annotations"]
+        for ann in annotations:
+            image_id_to_ann_data[image_id_to_name[ann["image_id"]]].append(ann["bbox"])
+
+        progress = sly.Progress("Create dataset {}".format(ds_name), len(image_id_to_ann_data))
+
+        images_names = os.listdir(curr_images_path)
+
+        for images_names_batch in sly.batched(images_names, batch_size=batch_size):
+            img_pathes_batch = [
+                os.path.join(curr_images_path, im_name) for im_name in images_names_batch
+            ]
+
+            img_infos = api.image.upload_paths(dataset.id, images_names_batch, img_pathes_batch)
+            img_ids = [im_info.id for im_info in img_infos]
+
+            anns = [create_ann(image_path) for image_path in img_pathes_batch]
+            api.annotation.upload_anns(img_ids, anns)
+
+            progress.iters_done_report(len(images_names_batch))
+
+    return project
